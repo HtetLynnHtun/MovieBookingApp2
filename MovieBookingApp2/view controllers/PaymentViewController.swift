39c80fa2235901a7117f73c9cc82ff3f9302db96
Htet Lynn Htun <sainount2012@gmail.com>
2022-02-20 12:00:43 +0630

    Finish skeleton of Payment Screen

---
+++ MovieBookingApp2/view controllers/PaymentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PaymentViewController.swift
+//  MovieBookingApp2
+//
+//  Created by kira on 20/02/2022.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+
+    @IBOutlet weak var textFieldCardNumber: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
